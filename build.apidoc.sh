585eefffee41b0f3e653f50b74203bb11aacf9d0
Lalit Tanwar <lalittanwar@Lalit-ka-MacBook-Pro.local>
2021-07-28 00:37:19 +0530

    file generator

--- build.apidoc.sh
+++ build.apidoc.sh
@@ -1,5 +1,5 @@
 mkdir -p server-xms 
-cd server-xms
-curl -GET http://127.0.0.1:8085/xms/v2/api-docs -o api-docs.json
-spectacle api-docs.json
-cd ..
+#cd server-xms
+curl -GET http://127.0.0.1:8085/xms/v2/api-docs -o server-xms/api-docs.json
+node bin/spectacle  server-xms/api-docs.json -t server-xms/public -f server-xms/public/index.html 
+#cd ..
