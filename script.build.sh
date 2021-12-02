5bae6c206b29a8299de70d9d18459d197dda8246
lalit.tanwar <lalit.tanwar07@gmail.com>
2021-12-02 14:38:18 +0530

    build

--- script.build.sh
+++ script.build.sh
@@ -1,5 +1,5 @@
 mkdir -p server-xms 
 #cd server-xms
-curl -GET http://127.0.0.1:8080/xms/v2/api-docs -o server-xms/api-docs.json
+curl -GET https://app.mehery.io/xms/v2/api-docs -o server-xms/api-docs.json
 node bin/spectacle  server-xms/api-docs.json -t server-xms/public -f index.html 
 #cd ..
