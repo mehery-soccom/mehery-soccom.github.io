32bd8a8cfebda7754b25e4e97f4b450e601eb13d
lalit.tanwar <lalit.tanwar07@gmail.com>
2022-05-23 10:42:52 +0530

    added c#

--- script.build.sh
+++ script.build.sh
@@ -9,7 +9,8 @@ rm server-xms/api-docs-raw.json
 
 node bin/spectacle  server-xms/api-docs.json -t server-xms/public -f index.html 
 
-widdershins --summary server-xms/api-docs.json -o reslate/source/index.md
+widdershins --summary server-xms/api-docs.json -o reslate/source/index.md \
+--language_tabs 'shell:Shell' 'http:HTTP' 'javascript:JS'  'python:Python' 'ruby:Ruby'  'php:PHP' 'java:Java'  'go:Go' 'csharp:C#'
 
 cd reslate
 npm i
